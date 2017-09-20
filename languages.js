0544bf53246d050fae34c73ba91a1dacddde4cac
Garcia Mendoza <c.garcia.mendoza@accenture.com>
2017-09-20 11:11:10 -0500

    Remove Angular leftovers.

--- languages.js
+++ languages.js
@@ -1,37 +1,37 @@
-var defaultLang = currentLang = "es",
-text,
-currentLangIndex = 0,
-langDefs = {
-  es: {
-    hola: "Hola",
-    prueba: "Prueba"
-  },
-  en: {
-    hola: "Hello",
-    prueba: "Test"
-  },
-  jp: {
-    hola: "Ohaiyo",
-    prueba: "Testo"
-  }
+var defaultLang = "es",
+	currentLang;
+
+var langDefs = {
+	es: {
+		hola: "Hola",
+		prueba: "Prueba"
+	},
+	en: {
+		hola: "Hello",
+		prueba: "Test"
+	},
+	jp: {
+		hola: "Ohaiyo",
+		prueba: "Testo"
+	}
 };
 
-text = langDefs[defaultLang];
+currentLang = langDefs[defaultLang];
 
 window.onload = loadLanguage(defaultLang);
 
 function chooseLang(chosenLang) {
-  text = langDefs[chosenLang];
+	currentLang = langDefs[chosenLang];
 }
 
 function loadLanguage(chosenLang) {
-  chooseLang(chosenLang);
+	chooseLang(chosenLang);
 
-  textElements = document.querySelectorAll("[data-lang]");
-  // alert(textElements.toString());
+	textElements = document.querySelectorAll("[data-lang]");
+	// alert(textElements.toString());
 
-  textElements.forEach(function(item, index) {
-    content = text[item.getAttribute('data-lang')];
-    item.textContent = content;
-  })
+	textElements.forEach(function (item, index) {
+		content = currentLang[item.getAttribute('data-lang')];
+		item.textContent = content;
+	})
 }
