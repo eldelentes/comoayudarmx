e3a8f4dcd5906aa850a6bc7a4114c277bdfdd963
Eduardo Higareda <eldelentes@gmail.com>
2017-09-20 00:15:27 -0500

    Add FAV Card

--- cards.js
+++ cards.js
@@ -1,4 +1,5 @@
 Cards = [
   //{title: "Donación Topos", description: "Los Topos son una asosiación de rescatistas indepe…tencilios necesarios para el rescate de personas.", type: "Monetaria", location: "Todo el mundo.", link: "https://twitter.com/topos/status/908000616575680512"},
-  {title: "Centro Acopio FARQ", description: "Centro de Acopio en La Facultar de Arquitectura UANL", type: "viveres", location: "Monterrey.", link: "https://twitter.com/qt_ewok/status/910358246657073153"},
+  {title: "Centro Acopio FARQ", description: "Centro de Acopio en La Facultar de Arquitectura UANL", type: "Viveres", location: "Monterrey.", link: "https://twitter.com/qt_ewok/status/910358246657073153"},
+  {title: "Centro Acopio FAV", description: "Centro de Acopio en La Facultar de Artes Visuales UANL", type: "Viveres", location: "Monterrey.", link: "https://scontent.fntr3-1.fna.fbcdn.net/v/t1.0-0/s480x480/21616244_1344314475691495_4475004746040701212_n.jpg?oh=d6ed0e1262d913fc70ce69694e190a52&oe=5A48CE3F"},
 ];
