c126c8958dcbb56282e1104015e9ec81d365f1b3
bhserna <bhserna@gmail.com>
2017-09-19 22:53:56 -0500

    Filter cards
    Adds the ability to filter the cards via two select controls, one for
    the donation type and the other for the location.
    
    As we don't have yet formal categories, the selects are populated with
    the values in the cards at initialization.

--- app.js
+++ app.js
@@ -1,10 +1,59 @@
 $('.modal__link').click(function(e){
-	e.preventDefault();
-	var modal = $(this).data('modal');
-	$('.modal[data-modal="' + modal + '"]').addClass('show');
+  e.preventDefault();
+  var modal = $(this).data('modal');
+  $('.modal[data-modal="' + modal + '"]').addClass('show');
 })
 
 $('.modal__close').click(function(e){
-	e.preventDefault();
-	$(this).parent().removeClass('show');
+  e.preventDefault();
+  $(this).parent().removeClass('show');
 })
+
+
+var handleFilterChange = function(e){
+  var type = $("#donation_type").val()
+  var location = $("#location").val()
+
+  var cardMatchType = function($card) {
+    return (!type || $card.find(".card__type").text().includes(type))
+  }
+
+  var cardMatchLocation = function($card) {
+    return (!location || $card.find(".card__location").text().includes(location))
+  }
+
+  var cardMatchFilters = function($card) {
+    return cardMatchType($card) && cardMatchLocation($card);
+  }
+
+  $(".card").each(function() {
+    var $card = $(this);
+
+    if (cardMatchFilters($card)) {
+      $card.show();
+    } else {
+      $card.hide();
+    }
+  })
+}
+
+var populateFilters = function(e) {
+  var populateFilter = function(selectorInCard, filterSelector) {
+    $(selectorInCard).each(function() {
+      var option = $(this).text();
+      var $select = $(filterSelector);
+      var $option = $("<option>" + option + "</option>");
+
+      if (!$select.text().includes(option)) {
+        $select.append($option);
+      }
+    });
+  }
+
+  populateFilter(".card__type h3", "#donation_type");
+  populateFilter(".card__location h3", "#location");
+}
+
+$(document).on("change", "#donation_type", handleFilterChange);
+$(document).on("change", "#location", handleFilterChange);
+$(document).ready(populateFilters);
